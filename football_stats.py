d38d61c8a751ae8bd7fc48ace07898abbe88129d
Pamela Del Rosario <pamela.delrosario@gmail.com>
2021-04-12 11:00:27 -0400

    Updated with code

--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,30 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import pandas as pd
+
+def main():
+    url = 'https://www.cbssports.com/nfl/stats/player/scoring/nfl/regular/all/?sortcol=rutd&sortdir=descending'
+    response = urllib.request.urlopen(url)
+    soup = BeautifulSoup(response.read(), features='lxml')
+    player_info = soup.find_all('tr')[1:21]
+    top_twenty = []
+
+    for row in player_info:
+        player_stats = {}
+        player_name = row.select('a')[1].contents[0]
+        player_position = row.select('span')[2].contents[0].strip()
+        player_team = row.select('span')[3].contents[0].strip()
+        rushing_touchdowns = row.select('td')[2].contents[0].strip()
+        player_stats['Name'] = player_name
+        player_stats['Position'] = player_position
+        player_stats['Team'] = player_team
+        player_stats['Rushing Touchdowns'] = rushing_touchdowns
+        top_twenty.append(player_stats)
+
+    player_info = pd.DataFrame(top_twenty)
+    player_info.index += 1
+    print(player_info)
+
+
 if __name__ == "__main__":
-    pass
+    main()
--- nfl_spreads.py
+++ nfl_spreads.py
@@ -1,2 +1,32 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import pandas as pd
+
+def main():
+    url = 'http://www.footballlocks.com/nfl_point_spreads.shtml'
+    response = urllib.request.urlopen(url)
+
+    soup = BeautifulSoup(response.read(), features='lxml')
+    table = soup.find('table', attrs={'cols': '4'})
+    games = table.find_all('tr')[1:]
+    current_spreads = []
+
+    for row in games:
+        details = row.find_all('td')
+        DAT = details[0].contents[0]
+        favorite = details[1].contents[0]
+        spread = details[2].contents[0]
+        underdog = details[3].contents[0]
+        game_info = {}
+        game_info['Game Date and Time'] = DAT
+        game_info['Favorite'] = favorite
+        game_info['Spread'] = spread
+        game_info['Underdog'] = underdog
+        current_spreads.append(game_info)
+
+    NFL_point_spreads = pd.DataFrame(current_spreads)
+    print(NFL_point_spreads.to_string(index=False))
+
+
 if __name__ == "__main__":
-    pass
+    main()
